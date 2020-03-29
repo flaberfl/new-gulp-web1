acd6447cf916bff2934adcabad6b9976bb336861
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-29 23:53:04 +0300

    testing home

---
+++ dist/assets/js/app.js
@@ -0,0 +1,6 @@
+// //= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+
+console.log("tabs");
+console.log("modal");
+// //= components/slick.js
---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+// //= ../../../node_modules/bootstrap/js/dist/util.js
+// //= ../../../node_modules/bootstrap/js/dist/modal.js
+
+//= components/tabs.js
+//= components/modal.js
+// //= components/slick.js
