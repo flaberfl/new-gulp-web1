f63aa385f4d74b3ec863816933cf574c8f02c695
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-30 11:55:01 +0300

    1

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,5 +1,5 @@
 // //= ../../../node_modules/bootstrap/js/dist/util.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
+// //= ../../../node_modules/bootstrap/js/dist/modal.js
 
 console.log("tabs");
 console.log("modal");
