7e74f8dab1c712a56173eaa2c6d10249c3285a29
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-30 12:12:19 +0300

    1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
-// //= ../../../node_modules/bootstrap/js/dist/util.js
-// //= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
 
 //= components/tabs.js
 //= components/modal.js
-// //= components/slick.js
+//= components/slick.js
